b8c0564868aad0bd33949a1ddab16fb7c5f5265d
starryiu <2283335802@qq.com>
2023-05-09 19:23:54 +0800

    更新页脚信息

--- src/app/shared/components/starryiu-footer/starryiu-footer.component.ts
+++ src/app/shared/components/starryiu-footer/starryiu-footer.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-starryiu-footer',
   templateUrl: './starryiu-footer.component.html',
-  styleUrls: ['./starryiu-footer.component.scss']
+  styleUrls: ['./starryiu-footer.component.scss'],
 })
 export class StarryiuFooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  nowYear = 0;
 
   ngOnInit(): void {
+    this.nowYear = new Date().getFullYear();
   }
-
 }
