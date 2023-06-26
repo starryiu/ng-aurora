49593a353f7e71d506d1351aa579251604d0e086
starryiu <2283335802@qq.com>
2023-06-26 15:43:50 +0800

    更改文章图片加载图

--- src/app/pipes/marked.pipe.ts
+++ src/app/pipes/marked.pipe.ts
@@ -7,16 +7,19 @@ const renderer = {
   image: (href: string, title: string | null, text: string | null) => {
     return `<div class="article-image-box">
       <img class="article-image" src="${href}" alt="${text}">
-      <span class="article-image-loading"">
-          <div class="loader21 loader">
-            <div class="loader-21">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-          </div>
+      <span class="article-image-loading">
+        <div class="wave-loading">
+            <span style="--time:1">L</span>
+            <span style="--time:2">o</span>
+            <span style="--time:3">a</span>
+            <span style="--time:4">d</span>
+            <span style="--time:5">i</span>
+            <span style="--time:6">n</span>
+            <span style="--time:7">g</span>
+            <span style="--time:8">.</span>
+            <span style="--time:9">.</span>
+            <span style="--time:10">.</span>
+        </div>
       </span>
     </div>`;
   },
