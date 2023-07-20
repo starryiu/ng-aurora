a5c38406471c9ced4d82ccdc656b5109257fa716
starryiu <2283335802@qq.com>
2023-07-20 15:50:40 +0800

    其它页面也添加giscus评论

--- src/app/components/starryiu-comment/starryiu-comment.component.ts
+++ src/app/components/starryiu-comment/starryiu-comment.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import 'giscus';
 
 @Component({
   selector: 'app-starryiu-comment',
   templateUrl: './starryiu-comment.component.html',
   styleUrls: ['./starryiu-comment.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class StarryiuCommentComponent implements OnInit {
   constructor() {}
