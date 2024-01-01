2fcd2ff7e084e58261d84468bb8f3bc8c03d291a
starryiu <2283335802@qq.com>
2024-01-01 21:55:33 +0800

    再次取消字体预加载

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,39 +22,8 @@ export class AppComponent implements OnInit {
     __config.images.sakura,
     __config.images.backTopCover,
   ];
-  private loadSiteFont = () => {
-    return new Promise<{ status: string; msg: string }>((resolve) => {
-      if (window.FontFace) {
-        const fontFile = new FontFace(
-          'CangErFeiBaiW02',
-          'url(assets/font/仓耳非白/仓耳非白W02.ttf)',
-        );
-        fontFile
-          .load()
-          .then(() => {
-            resolve({
-              status: 'success',
-              msg: '网站字体加载成功！！！',
-            });
-          })
-          .catch(() => {
-            resolve({
-              status: 'error',
-              msg: '网站标题字体加载失败',
-            });
-          });
-      } else {
-        resolve({
-          status: 'error',
-          msg: '浏览器不支持 FontFace',
-        });
-      }
-    });
-  };
 
   async ngOnInit() {
-    const loadSiteFontStatus = await this.loadSiteFont();
-    console.log(loadSiteFontStatus.msg);
     this.utilsService.resetSiteTitle();
     this.utilsService.addMetaTag({
       name: 'author',
