53f75536d9033ccd9663e2c15a427bbe9ed53439
starryiu <2283335802@qq.com>
2024-01-01 20:37:34 +0800

    取消字体预加载

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ViewEncapsulation } from '@angular/core';
-import { timeout, forkJoin, timer } from 'rxjs';
+import { timeout, forkJoin } from 'rxjs';
 import { UtilsService } from './shared/utils.service';
 import __config from '../config';
 
@@ -23,35 +23,6 @@ export class AppComponent implements AfterViewInit {
     __config.images.backTopCover,
   ];
 
-  private loadSiteFont = () => {
-    return new Promise<{ status: string; msg: string }>((resolve) => {
-      if (window.FontFace) {
-        const fontFile = new FontFace(
-          'CangErFeiBaiW02',
-          'url(assets/font/仓耳非白/仓耳非白W02.ttf)',
-        );
-        fontFile
-          .load()
-          .then(() => {
-            resolve({
-              status: 'success',
-              msg: '网站字体加载成功！！！',
-            });
-          })
-          .catch(() => {
-            resolve({
-              status: 'error',
-              msg: '网站标题字体加载失败',
-            });
-          });
-      } else {
-        resolve({
-          status: 'error',
-          msg: '浏览器不支持 FontFace',
-        });
-      }
-    });
-  };
   ngAfterViewInit(): void {
     this.utilsService.resetSiteTitle();
     this.utilsService.addMetaTag({
@@ -87,11 +58,7 @@ export class AppComponent implements AfterViewInit {
             console.error('预加载图片失败，超时！');
           },
           complete: async () => {
-            const loadSiteFontResult = await this.loadSiteFont();
-            timer(100).subscribe(() => {
-              console.log(loadSiteFontResult.msg);
-              this.showSite = true;
-            });
+            this.showSite = true;
           },
         });
     };
